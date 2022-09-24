bb2493b3ba587174b4f7528b941aeb90e18b7a2d
Antonov Kirill <antonovkir2@gmail.com>
2022-09-24 18:57:38 +0500

    add  move left action

--- script.js
+++ script.js
@@ -6,26 +6,42 @@ let imgBlockPosition = 0;
 
 // ФУНКЦИИ
 const rightHandler = () => {
+    heroImg.style.transform = 'scale(-1,1)'
     rightPosition++
     imgBlockPosition++
     if (rightPosition > 5) rightPosition = 0
-    heroImg.style.left = `-${rightPosition*288}px`
-    imgBlock.style.left = `${imgBlockPosition*20}px`
+    heroImg.style.left = `-${rightPosition * 288}px`
+    imgBlock.style.left = `${imgBlockPosition * 20}px`
+}
+
+const leftHandler = () => {
+    heroImg.style.transform = 'scale(1,1)'
+    rightPosition++
+    imgBlockPosition--
+    if (rightPosition > 5) rightPosition = 0
+    heroImg.style.left = `-${rightPosition * 288}px`
+    imgBlock.style.left = `${imgBlockPosition * 20}px`
 }
 
 // ОБРАБОТЧИКИ СОБЫТИЙ
 let timer = null
+let x = 0
+let halfWidth = window.screen.width / 2
 const onTouchStart = (event) => {
- timer = setInterval(() => {
-     rightHandler()
- }, 130)
+    event.preventDefault()
+    x = event.type === 'mousedown' ? event.screenX : event.touches[0].screenX
+
+    timer = setInterval(() => {
+        (x > halfWidth) ? rightHandler() : leftHandler()
+    }, 130)
 }
 
 const onTouchEnd = (event) => {
+    event.preventDefault()
     clearInterval(timer)
 }
 
-window.onmousedown = (event) => onTouchStart
-window.ontouchstart = (event) => onTouchStart
-window.onmouseup = (event) => onTouchEnd
-window.ontouchend = (event) => onTouchEnd
+window.onmousedown = (event) => onTouchStart(event)
+window.ontouchstart = (event) => onTouchStart(event)
+window.onmouseup = (event) => onTouchEnd(event)
+window.ontouchend = (event) => onTouchEnd(event)
