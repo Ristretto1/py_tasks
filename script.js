ade1e0bbdf75ed153e7a4dac038f3dcef897a9bb
Antonov Kirill <antonovkir2@gmail.com>
2022-09-24 23:14:56 +0500

    add jump and hit actions

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // ПЕРЕМЕННЫЕ
+let rightPosition = 0;
+let imgBlockPosition = 0;
+let direction = 'right'
+let hit = false
+let jump = false
+let timer = null
+let x = 0
+let halfWidth = window.screen.width / 2
+
 let canvas = window.document.querySelector('#canvas');
+let jumpBlock = window.document.querySelector('#jump-block');
+let hitBlock = window.document.querySelector('#hit-block');
 let fsBtn = window.document.querySelector('#fsBtn');
+let heroImg = window.document.querySelector('#hero-img');
+let imgBlock = window.document.querySelector('#img-block');
+
+hitBlock.style.top = `${window.screen.height / 2 - 144 / 2}px`
+jumpBlock.style.top = `${window.screen.height / 2 - 144 / 2}px`
+
+heroImg.onclick = (event) => {
+    event.preventDefault()
+}
 fsBtn.onclick = () => {
     if (window.document.fullscreen) {
         fsBtn.src = 'assets/img/fullscreen.png'
@@ -10,15 +30,8 @@ fsBtn.onclick = () => {
         canvas.requestFullscreen()
     }
 }
-let heroImg = window.document.querySelector('#hero-img');
-heroImg.onclick = (event) => {
-    event.preventDefault()
-}
-let imgBlock = window.document.querySelector('#img-block');
-let rightPosition = 0;
-let imgBlockPosition = 0;
-let direction = 'right'
-
+jumpBlock.onclick = () => jump = true
+hitBlock.onclick = () => hit = true
 // ФУНКЦИИ
 const rightHandler = () => {
     heroImg.style.transform = 'scale(-1,1)'
@@ -44,7 +57,7 @@ const standHandler = () => {
     switch (direction) {
         case 'right': {
             heroImg.style.transform = 'scale(-1,1)'
-            if (rightPosition > 4) rightPosition = 0
+            if (rightPosition > 4) rightPosition = 1
             break;
         }
         case 'left': {
@@ -52,7 +65,8 @@ const standHandler = () => {
             if (rightPosition > 3) rightPosition = 0
             break;
         }
-        default: break
+        default:
+            break
     }
 
     rightPosition++
@@ -60,15 +74,61 @@ const standHandler = () => {
     heroImg.style.top = '0'
 }
 
-// ОБРАБОТЧИКИ СОБЫТИЙ
-let timer = null
-const lifeCycle = () => {
-    timer = setInterval(() => {
-        standHandler()
-    }, 150)
+const hitHandler = () => {
+    switch (direction) {
+        case 'right': {
+            heroImg.style.transform = 'scale(-1,1)'
+            if (rightPosition > 4) {
+                rightPosition = 0
+                hit = false
+            }
+            break;
+        }
+        case 'left': {
+            heroImg.style.transform = 'scale(1,1)'
+            if (rightPosition > 3) {
+                rightPosition = 0
+                hit = false
+            }
+            break;
+        }
+        default:
+            break
+    }
+
+    rightPosition++
+    heroImg.style.left = `-${rightPosition * 288}px`
+    heroImg.style.top = '-864px'
 }
-let x = 0
-let halfWidth = window.screen.width / 2
+
+const jumpHandler = () => {
+    switch (direction) {
+        case 'right': {
+            heroImg.style.transform = 'scale(-1,1)'
+            if (rightPosition > 4) {
+                rightPosition = 0
+                jump = false
+            }
+            break;
+        }
+        case 'left': {
+            heroImg.style.transform = 'scale(1,1)'
+            if (rightPosition > 3) {
+                rightPosition = 0
+                jump = false
+            }
+            break;
+        }
+        default:
+            break
+    }
+
+    rightPosition++
+    heroImg.style.left = `-${rightPosition * 288}px`
+    heroImg.style.top = '-288px'
+}
+
+// ОБРАБОТЧИКИ СОБЫТИЙ
 const onTouchStart = (event) => {
     clearInterval(timer)
     x = event.type === 'mousedown' ? event.screenX : event.touches[0].screenX
@@ -92,6 +152,18 @@ window.onmousedown = (event) => onTouchStart(event)
 window.ontouchstart = (event) => onTouchStart(event)
 window.onmouseup = (event) => onTouchEnd(event)
 window.ontouchend = (event) => onTouchEnd(event)
+
+const lifeCycle = () => {
+    timer = setInterval(() => {
+        if (hit) {
+            hitHandler()
+        } else if (jump) {
+            jumpHandler()
+        } else {
+            standHandler()
+        }
+    }, 150)
+}
 
 const start = () => {
     lifeCycle()
