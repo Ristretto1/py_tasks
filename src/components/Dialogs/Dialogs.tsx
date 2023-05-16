8d12af379b82f374a037726f5a93b7f587dfb562
k.antonov <antonovkir2@gmail.com>
2023-05-16 09:07:17 +0500

    unit 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
 
 export const Dialogs = () => {
+
+    const styleDialogsWrapper = {
+        display: 'flex'
+    }
+    const styleDialogs = {
+        width: '150px'
+    }
+    const styleDialogsActive = {
+        color: 'white'
+    }
+    const styleDialogsItem = {
+        padding: '5px'
+    }
+    const styleMessagesItem = {
+        padding: '5px'
+    }
+
     return (
-        <div>
-            Dialogs
+        <div className={'dialogs_wrapper'} style={styleDialogsWrapper}>
+            <div className={'dialogs'} style={styleDialogs}>
+                <div className={'dialogs_item'} style={styleDialogsItem}>Kirill</div>
+                <div className={'dialogs_item'} style={{...styleDialogsItem, ...styleDialogsActive}}>Tayson</div>
+                <div className={'dialogs_item'} style={styleDialogsItem}>Anya</div>
+            </div>
+            <div className="messages">
+                <div className="messages_item" style={styleMessagesItem}>123</div>
+                <div className="messages_item" style={styleMessagesItem}>456</div>
+                <div className="messages_item" style={styleMessagesItem}>789</div>
+                <div className="messages_item" style={styleMessagesItem}>789</div>
+                <div className="messages_item" style={styleMessagesItem}>789</div>
+            </div>
         </div>
     );
 };
