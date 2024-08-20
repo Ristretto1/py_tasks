8ad113d27d4d5730669f962daeadb3d83149d789
Kirill Antonov <k.antonov@happynumbers.com>
2024-08-20 16:19:32 +0500

    feat: add hw10

---
+++ hw10/authenticator.py
@@ -0,0 +1,72 @@
+import os
+from datetime import datetime
+from typing import Optional
+
+from hw9.exceptions import AuthorizationError, RegistrationError
+
+
+class Authenticator:
+    def __init__(self):
+        self.login: Optional[str] = None
+        self._password: Optional[str] = None
+        self.last_success_login_at: Optional[datetime] = None
+        self.errors_count: int = 0
+
+        if self._is_auth_file_exist():
+            self._read_auth_file()
+
+    @staticmethod
+    def _is_auth_file_exist() -> bool:
+        """Метод проверяет файл, что он существует"""
+
+        return os.path.exists('./auth.txt')
+
+    def _read_auth_file(self):
+        """Метод, читающий авторизационный файл и сохраняет их"""
+
+        with open('./auth.txt') as f:
+            self.login = f.readline().strip()
+            self._password = f.readline().strip()
+            self.last_success_login_at = datetime.fromisoformat((f.readline().strip()))
+            self.errors_count = int(f.readline().strip())
+
+    def _update_auth_file(self):
+        """Метод, перезаписывающий авторизационные данные"""
+
+        with open('./auth.txt', 'w') as f:
+            f.write(f'{self.login}\n')
+            f.write(f'{self._password}\n')
+            f.write(f'{datetime.utcnow().isoformat()}\n')
+            f.write(f'{self.errors_count}')
+
+    def registrate(self, login, password):
+        """Метод, позволяющий провести регистрацию по логину и паролю"""
+
+        if self._is_auth_file_exist():
+            raise RegistrationError('auth file exist')
+
+        if self.login is not None:
+            self.errors_count += 1
+            raise RegistrationError('login is not None')
+
+        self.login = login
+        self._password = password
+        self.last_success_login_at = datetime.utcnow()
+        self._update_auth_file()
+
+    def authorize(self, login: str, password: str):
+        """Метод, позволяющий провести авторизацию по логину и паролю"""
+
+        if not self._is_auth_file_exist():
+            raise AuthorizationError('auth file don\'t exist')
+
+        if not self.login:
+            self.errors_count += 1
+            raise AuthorizationError('login is None')
+
+        if login != self.login or password != self._password:
+            self.errors_count += 1
+            raise AuthorizationError('Неверный логин или пароль')
+
+        # self.errors_count = 0
+        self._update_auth_file()
---
+++ hw10/main.py
@@ -0,0 +1,83 @@
+"""Домашка в отдельном файле рядом"""
+
+from random import randrange
+from typing import Optional
+
+from hw7.exceptions import ValidationError
+from hw7.validator import Validator, DataWithDate
+from hw9.authenticator import Authenticator
+from hw9.exceptions import AuthorizationError, RegistrationError
+
+
+def guess_number_game():
+    """Игра Угадай число от 0 до 5"""
+
+    right_answer = randrange(0, 6)
+    count_game_tries = 0
+
+    while True:
+        guess = input('Угадай число от 0 до 5: ').strip()
+
+        try:
+            guess = int(guess)
+        except ValueError:
+            print(f'Я поймал ошибку: Это не число')
+            count_game_tries += 1
+            continue
+
+        if int(guess) != right_answer:
+            print(f'Нет, попробуй ещё')
+            count_game_tries += 1
+            continue
+
+        print(f'Да, ты угадал! Это действительно {guess}. Ты потратил {count_game_tries + 1} попытки(-ок)')
+        break
+
+
+def auth_loop(func):
+    def wrapper():
+        while True:
+            result = func()
+            if result:
+                break
+
+    return wrapper
+
+
+@auth_loop
+def main():
+    authenticator = Authenticator()
+
+    is_user_exist = authenticator.login
+
+    if not is_user_exist:
+        print('Вы проходите регистрацию')
+    else:
+        print('Чтобы авторизоваться нужно ввести логин и пароль')
+
+    input_login = input('Введите логин: ')
+    input_password = input('Введите пароль: ')
+
+    if is_user_exist:
+        try:
+            authenticator.authorize(input_login, input_password)
+        except AuthorizationError as e:
+            print(f'Ошибка: {e}')
+            return False
+
+        print(f'\nВаш логин: {authenticator.login}')
+        print(f'Успешная авторизация в: {authenticator.last_success_login_at.strftime("%d.%m.%Y %H:%M:%S")}')
+        print(f'{authenticator.errors_count} раз(а) пытались войти в приложение с ошибкой авторизации.')
+
+        guess_number_game()
+        return True
+
+
+    try:
+        authenticator.registrate(input_login, input_password)
+        print('Вы успешно зарегистрировались. Требуется авторизоваться')
+    except RegistrationError as e:
+        print(f'Ошибка: {e}')
+        return False
+
+main()
